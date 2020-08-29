66e4a5f35378aaf4022e244d8cc18971486deca1
Ville <visavola@gmail.com>
2020-08-29 23:18:22 +0300

    Added build

---
+++ build/precache-manifest.b7f3ee9683221a469985ab6083a1ffb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621f43baac16ea015538e82311aef968",
+    "url": "/VilleSavolainen1/Stopwatch/index.html"
+  },
+  {
+    "revision": "e54dca85d687800e0ea1",
+    "url": "/VilleSavolainen1/Stopwatch/static/css/main.cfcd8a21.chunk.css"
+  },
+  {
+    "revision": "eb328786307cc12c7e2b",
+    "url": "/VilleSavolainen1/Stopwatch/static/js/2.399c1457.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VilleSavolainen1/Stopwatch/static/js/2.399c1457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e54dca85d687800e0ea1",
+    "url": "/VilleSavolainen1/Stopwatch/static/js/main.b52ee53e.chunk.js"
+  },
+  {
+    "revision": "94fb3a562cd80cd357e0",
+    "url": "/VilleSavolainen1/Stopwatch/static/js/runtime-main.3ff961f5.js"
+  }
+]);
