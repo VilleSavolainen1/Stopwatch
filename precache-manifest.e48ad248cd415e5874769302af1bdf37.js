237203a9a801cdf2f9de55a03b38e1d22e57d422
Ville <visavola@gmail.com>
2020-08-29 23:24:24 +0300

    Updates

---
+++ precache-manifest.e48ad248cd415e5874769302af1bdf37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96d3110aee843d7b800860e172d8321a",
+    "url": "/Stopwatch/index.html"
+  },
+  {
+    "revision": "e54dca85d687800e0ea1",
+    "url": "/Stopwatch/static/css/main.cfcd8a21.chunk.css"
+  },
+  {
+    "revision": "eb328786307cc12c7e2b",
+    "url": "/Stopwatch/static/js/2.399c1457.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Stopwatch/static/js/2.399c1457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e54dca85d687800e0ea1",
+    "url": "/Stopwatch/static/js/main.b52ee53e.chunk.js"
+  },
+  {
+    "revision": "69c1d48c64e7171e78f1",
+    "url": "/Stopwatch/static/js/runtime-main.c7f13b24.js"
+  }
+]);
